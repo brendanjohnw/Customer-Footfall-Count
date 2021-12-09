df32b6bf3fe6a372789371c2d25dad458a5fd052
Brendan John Lee <79955754+brendanjohnw@users.noreply.github.com>
2021-12-09 15:15:49 +0800

    Fixed Time Counter

--- ObjTrack.py
+++ ObjTrack.py
@@ -1,10 +1,12 @@
 import cv2
+import math
+
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import tracker
 import time
 
-init_time = float(time.time())
 
 
 #Load Video Image
@@ -14,8 +16,6 @@ def HOG_initial_count():
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     _, first_frame = video.read()
-    frame_height = first_frame.shape[0]
-    frame_width = first_frame.shape[1]
     (first_frame_region, _) = hog.detectMultiScale(first_frame, winStride=(3, 3), padding=(8, 8), scale=1.05)
     return len(first_frame_region)
 codec = cv2.VideoWriter_fourcc(*'XVID')
@@ -44,6 +44,10 @@ conf_threshold = 0.40
 nms_threshold = 0.40
 seconds = 0
 exact_seconds = 0
+init_time = 0
+total_frames = 0
+total_fps = 0
+
 seconds_person_counts = {}
 seconds_person_enter = {}
 seconds_person_exit = {}
@@ -52,14 +56,20 @@ seconds_person_exit = {}
 # Instantiating the tracker
 tracker = tracker.EuclideanDistTracker()
 
+# For generating approximated data
+def time_estimator(average):
+    runtime_seconds = time.time() - init_time
+    return math.floor(int(runtime_seconds) / average)
+
 pre_obj = {}
 down = int(0)
 up = int(0)
 total = int(0)
-init_time = time.localtime()
+init_time = time.time()
 total_counts = HOG_initial_count()
 try:
     while True:
+        total_frames += 1
         _, img = video.read()
 
         if img is None:
@@ -95,6 +105,8 @@ try:
 
             color = colors[int(id) % len(colors)]
             color = [i * 255 for i in color]
+
+            cv2.circle(img, centroid_location, 3, (0,255,0), 2)
 
             cv2.rectangle(img, (x, y), (x + w, y + h), color=color, thickness=2)
 
@@ -133,15 +145,12 @@ try:
                     }
 
         fps = 1. / (time.time() - start_time)
-        seconds = seconds + round(fps)
-        exact_seconds = round(seconds/16)
+        total_fps +=fps
+        average_frames = total_fps/total_frames
+        exact_seconds = time_estimator(average_frames)
+
         seconds_person_counts[exact_seconds] = total_counts
-
-        print(seconds_person_counts)
-
-
-
-        #print(seconds_person_enter)
+        print(exact_seconds)
         cv2.putText(img, "FPS: {:.2f}".format(fps), (0,30), 0, 1, (0,0,255), 2)
         cv2.putText(img, "People Entering: " + str(up), (0, 60), 0, 0.5, (0, 0, 255), 1)
         cv2.putText(img, "People Exiting: " + str(down), (0,90), 0, 0.5, (0,0,255), 1)
@@ -152,15 +161,23 @@ try:
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             exit()
-except:
+
     import csv
+
     data = seconds_person_counts.items()
+    data_1 = seconds_person_enter.items()
+    data_2 = seconds_person_exit.items()
 
     with open('seconds_and_counts.csv', 'w') as out:
         csv_out = csv.writer(out)
         csv_out.writerow(['Second', 'Cumulative Count'])
         for row in data:
             csv_out.writerow(row)
+except:
+
+
+
+
     video.release()
     cv2.destroyAllWindows()
 
